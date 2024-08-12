db4e4c226b09b8e592973c9220d854ed2a1ffe19
Berlinwalll <vigiadams@gmail.com>
2024-08-12 13:22:45 -0400

    Delete Javascript directory

--- Javascript/index.js
+++
@@ -1,8 +0,0 @@
-function checkPassword() {
-    const password = document.getElementById('password').value;
-    if (password === 'avgvsst') {
-        window.location.href = 'birthday.html';
-    } else {
-        alert('Incorrect password. Please try again.');
-    }
-}
